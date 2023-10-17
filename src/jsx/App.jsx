beb0f9ee5c3386b549b138c4f7f8859a802e461a
Teemo Tebest <teemo.tebest@gmail.com>
2023-10-17 15:09:24 +0200

    Updated styles and added a input range

--- src/jsx/App.jsx
+++ src/jsx/App.jsx
@@ -6,6 +6,9 @@ import '../styles/styles.less';
 // https://d3js.org/
 import * as d3 from 'd3';
 
+// https://vis4.net/chromajs/
+import chroma from 'chroma-js';
+
 // Load helpers.
 import CSVtoJSON from './helpers/CSVtoJSON.js';
 // import formatNr from './helpers/FormatNr.js';
@@ -15,33 +18,92 @@ import CSVtoJSON from './helpers/CSVtoJSON.js';
 
 function App() {
   // Data states.
-  const [data, setData] = useState(false);
-  const [curYear, setCurYear] = useState(1990);
+  const endYear = 2023;
+  const startYear = 1990;
   const chartRef = useRef(null);
+  const interval = useRef(null);
   const svg = useRef(null);
   const x = useRef(null);
   const y = useRef(null);
+  const [curYear, setCurYear] = useState(startYear);
+  const [data, setData] = useState(false);
+
+  const f = chroma.scale(['rgba(221, 221, 221, 0.75)', '#009edb']).domain([0.27, 1]);
 
   const addData = useCallback((year) => {
-    // Add the line
+    // Remove and add existing lines
+    for (let i = startYear; i <= endYear; i++) {
+      svg.current.select(`path.line_${i}`).remove();
+    }
+    for (let i = (year - 1); i >= startYear; i--) {
+      svg.current.append('path')
+        .attr('class', `line line_${i}`)
+        .datum(data.filter(el => (el.year === i)))
+        .attr('fill', 'none')
+        .attr('stroke', '#000')
+        .attr('stroke-width', 1)
+        .attr('d', d3.line()
+          .x((d) => x.current(d.date.toLocaleString('default', { month: 'short' }).slice(0, 3)))
+          .y((d) => y.current(d.value)));
+    }
+    // Edit existing lines
     svg.current.selectAll('path.line').each((line, i, nodes) => {
-      d3.select(nodes[i]).attr('stroke', (d) => `rgba(0, 0, 0, ${(d[0].year - 1980) / (year - 1980) - 0.3})`).attr('stroke-width', 1);
+      // d3.select(nodes[i]).attr('stroke', (d) => `rgba(0, 0, 0, ${(1 - ((year - d[0].year) / 100) * 2) - 0.4})`).attr('stroke-width', 1);
+      d3.select(nodes[i]).attr('stroke', (d) => f(1 - ((year - d[0].year) / 100) * 2 - 0.4)).attr('stroke-width', 1);
     });
+    // Add the line
     svg.current.append('path')
-      .attr('class', 'line')
+      .attr('class', `line line_${year}`)
       .datum(data.filter(el => (el.year === year)))
       .attr('fill', 'none')
       .attr('stroke', '#009edb')
-      .attr('stroke-width', 4)
+      .attr('stroke-width', 5)
       .attr('d', d3.line()
         .x((d) => x.current(d.date.toLocaleString('default', { month: 'short' }).slice(0, 3)))
         .y((d) => y.current(d.value)));
-  }, [data]);
+  }, [data, f]);
+
+  const toggleInterval = () => {
+    if (interval !== null) {
+      interval.current = setInterval(() => {
+        document.querySelector('.play_pause_button').innerHTML = '⏸︎';
+        setCurYear(currentState => {
+          const newState = currentState + 1;
+          if (newState > endYear) {
+            clearInterval(interval.current);
+            interval.current = null;
+            document.querySelector('.play_pause_button').innerHTML = '⏵︎';
+            return currentState;
+          }
+          return newState;
+        });
+      }, 150);
+    }
+    // Clearing the interval
+    return () => {
+      interval.current = null;
+      clearInterval(interval.current);
+    };
+  };
+
+  const togglePlayPause = () => {
+    if (interval.current !== null) { // Pause
+      document.querySelector('.play_pause_button').innerHTML = '⏵︎';
+      clearInterval(interval.current);
+      interval.current = null;
+    } else { // Play
+      document.querySelector('.play_pause_button').innerHTML = '⏸︎';
+      if (curYear === endYear) {
+        setCurYear(startYear);
+      }
+      toggleInterval();
+    }
+  };
 
   const createChart = useCallback(() => {
     // set the dimensions and margins of the graph
     const margin = {
-      bottom: 30, left: 30, right: 20, top: 20
+      bottom: 30, left: 35, right: 20, top: 20
     };
     const height = 300 - margin.top - margin.bottom;
     const width = chartRef.current.offsetWidth - margin.left - margin.right;
@@ -56,12 +118,13 @@ function App() {
         `translate(${margin.left},${margin.top})`
       );
 
-    // Add X axis --> it is a date format
+    // Add X axis
     x.current = d3.scalePoint()
       .domain(['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'])
       .range([0, width]);
     svg.current.append('g')
       .attr('transform', `translate(0,${height})`)
+      .attr('class', 'axis axis_x')
       .call(d3.axisBottom(x.current));
 
     // Add Y axis
@@ -69,7 +132,9 @@ function App() {
       .domain([0, 180])
       .range([height, 0]);
     svg.current.append('g')
+      .attr('class', 'axis axis_y')
       .call(d3.axisLeft(y.current));
+    toggleInterval();
   }, []);
 
   useEffect(() => {
@@ -82,20 +147,12 @@ function App() {
     }
   }, [addData, curYear, data]);
 
-  useEffect(() => {
-    // Implementing the setInterval method
-    let interval;
-    if (curYear > 2022) {
-      clearInterval(interval);
-    } else {
-      interval = setInterval(() => {
-        setCurYear(curYear + 1);
-      }, 150);
-    }
-
-    // Clearing the interval
-    return () => clearInterval(interval);
-  }, [curYear]);
+  const changeYear = (event) => {
+    clearInterval(interval.current);
+    interval.current = null;
+    document.querySelector('.play_pause_button').innerHTML = '⏵︎';
+    setCurYear(parseInt(event.target.value, 10));
+  };
 
   useEffect(() => {
     const data_file = (window.location.href.includes('unctad.org')) ? '/sites/default/files/data-file/2023-food_price_index.csv' : './assets/data/2023-food_price_index.csv';
@@ -118,8 +175,19 @@ function App() {
   return (
     <div className="app">
       <div className="container">
-        <h3>FAO Food price Index</h3>
-        <h4>1990–2023</h4>
+        <div className="title_container">
+          <div className="title_logo_container">
+            <img src="./assets/img/unctad_logo.png" alt="Logo" />
+          </div>
+          <div className="title_text_container">
+            <h3>Food price Index</h3>
+            <h4>The FAO Food Price Index (FFPI) is a measure of the monthly change in international prices of a basket of food commodities, 1990–2023</h4>
+          </div>
+        </div>
+        <div className="play_controls">
+          <button type="button" className="play_pause_button" aria-label="Toggle play/pause" title="Toggle play/pause" onClick={(event) => togglePlayPause(event)} />
+          <input className="play_range" type="range" aria-label="Range" value={curYear} min={startYear} max={endYear} onInput={(event) => changeYear(event)} onChange={(event) => changeYear(event)} />
+        </div>
         <div className="chart_container">
           <div className="chart" ref={chartRef} />
           <div className="year">
